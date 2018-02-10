5dba533e04304c548d2255d9b7c370f449aa05ec
Quintessa Anderson <quintessaanderson@Quintessas-MacBook-Pro.local>
2018-02-09 21:41:36 -0600

    Fri night commit All commands working - Minus require fs command

--- liri.js
+++ liri.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-var Request = require('request');
+var request = require('request');
+// var fs = require('fs');
+// var path = require('path');
 var keys = require('./keys.js');
 // console.log(keys);
 
@@ -50,17 +52,70 @@ spotify.search({ type:'track', query: songTitle}, function (err, data) {
     var artist = data.tracks.items[0].album.artists[0].name
     console.log(artist);
    
+    var songUrl = data.tracks.items[0].album.external_urls.spotify
+    console.log(songUrl);
+
     var album = data.tracks.items[0].album.name
     console.log(album);
 
     var songTitle = data.tracks.items[0].name
     console.log(songTitle);
 
-    var songUrl = data.tracks.items[0].album.external_urls.spotify
-    console.log(songUrl);
-    
 });
 
 }
 
+if(process.argv[2] === "movie-this"){ 
 
+var movieName = process.argv[3];
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+
+if(!error && response.statusCode === 200) {
+
+    // console.log(body);
+    console.log("Movie Title:\n" + JSON.parse(body).Title);
+    console.log("This Movie Was Realsed in:\n" + JSON.parse(body).Year);
+    console.log("Actors:\n" + JSON.parse(body).Actors);
+    console.log("Language:\n" + JSON.parse(body).Language);
+    console.log("Country Movie Was Produced In:\n" + JSON.parse(body).Country);
+    console.log("Synopsis:\n" + JSON.parse(body).Plot);
+    console.log("This Movie's IMDB Rating Is:\n" + JSON.parse(body).imdbRating);
+    console.log("This Movie's Rotten Tomato Rating Is:\n" + JSON.parse(body).Ratings[1].Value);
+
+} 
+// else {
+//     console.log("Mr. Nobody");
+// }
+
+}) 
+}
+
+if (process.argv.length < 3) {
+    console.log('Usage: node' + process.argv[1] + 'FILENAME');
+    process.exit(1);
+  }
+
+// var fs = require('fs');
+// var textFile = process.argv[2]
+
+// fs.readFile("textFile", "utf8", function(err, data) {
+
+//     if(err) {
+//         return console.log(err);
+//     }
+
+//     console.log('OK:' + textFile);
+//     console.log(data);
+
+
+//         // fs.appendFileSync("random.txt", "random-text");
+
+//         // console.log(SAVED);
+    
+//     }
+// );
